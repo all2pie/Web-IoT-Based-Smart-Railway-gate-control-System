975c44f63fbd4081466ba19500c2e4b392e6960a
Muhammad Haris <trygames19@gmail.com>
2018-11-07 14:57:43 +0000

    Added Station and Gate Control

--- index.js
+++ index.js
@@ -1,9 +1,10 @@
 var express = require("express"),
     app     = express(),
-    mongo   = require("mongoose"),
+    fs      = require("fs"),
     parser  = require("body-parser"),
     http    = require('http').Server(app),
-    io      = require("socket.io")(http);
+    io      = require("socket.io")(http),
+    data ={};
     
     app.use(express.static("public"));    
     app.use(parser.urlencoded({extended:true}));
@@ -11,16 +12,45 @@ var express = require("express"),
     
     
     
-app.get("/",function (req,res) {
+    
+app.get("*",function (req,res) {
     res.render("home");
 });
 
+ 
+
 io.on('connection', function(socket){
-    console.log("got a connection")
+    
+    socket.on('web', function(auth){
+      fs.readFile("station", function read(err,data) {
+            if(err) {
+             return console.log(err);
+            }
+            if(data){
+                var loc = JSON.parse(data)
+                socket.emit("station",loc.lat,loc.lon);
+            }
+        });
+    });    
   socket.on('location', function(lat,lan,speed){
-      console.log(lat,lan,speed)
-      io.emit("loc",lat,lan,speed)
+      io.emit("loc",lat,lan,speed);
     });
+  socket.on('station',function (lat,lon) {
+      data.lat = lat;
+      data.lon = lon;
+      fs.writeFile("station", JSON.stringify(data, null, 2), function(err) {
+            if(err) {
+             return console.log(err);
+            }
+        }); 
+      io.emit("station",lat,lon);
+  });
+  socket.on("close",function(distance) {
+      io.emit("close",distance);
+  });
+  socket.on("open",function(distance) {
+      io.emit("open",distance);
+  });
 });
 
 http.listen(process.env.PORT,process.env.IP,function () {
--- public/scripts/map.js
+++ public/scripts/map.js
@@ -1,57 +1,67 @@
 var io = io(),
     map,
-    markers = [];
+    distance = 0,
+    station = {lat: 0.0, lng: 0.0},
+    trainMarkers =[],
+    stationMarkers=[],
+    closing = false;
+    
+    
 
 function initMap() {
   
   map = new google.maps.Map(document.getElementById('map'));
-
-io.on('connection', function(socket){
-    console.log("got a connection")
+  var bounds = new google.maps.LatLngBounds();
+  map.fitBounds(bounds);
+io.on('connect', function(){ 
+    io.emit("web","auth");
+    io.on('station', function(lat,lng){
+      station.lat = lat;
+      station.lng = lng;
+      addStation(lat,lng);
+    });
 });  
 
 io.on('loc', function(lat,lan,speed){
-    clearMarkers();
-    addMarker(lat,lan,speed);
-  	map.panTo({lat: lat, lng: lan});
-  	map.setZoom(20);
-  	
+    addTrain(lat,lan,speed);
+    console.log(stationMarkers,trainMarkers);
+    var current = new google.maps.LatLng({lat: lat, lng: lan});
+    var destination = new google.maps.LatLng(station);
+  	bounds.extend(current)
+    map.fitBounds(bounds);
+  	distance = google.maps.geometry.spherical.computeDistanceBetween(current,destination);
+  	console.log(distance)
+  	if (distance < 200) {
+  	  if(!closing){
+  	  io.emit("close",distance);
+  	  closing = true;
+  	  }
+  	}
+  	if(closing){
+  	  io.emit("open",distance);
+  	  closing = false;
+  	}
 });
 
-  
-  var bounds = new google.maps.LatLngBounds();
-  map.fitBounds(bounds);
-
-  var info = new google.maps.InfoWindow({
-  	content:"sfjsadfksjfksjfksadjfklasjf"
-  });
-
-  
-
-  
 }
 
-function clearMarkers() {
-  markers.forEach(function (marker) {
-    marker.setMap(null);
-  });
-  markers=[];
-}
 
-function addMarker(lat,lan,speed) {
-  var icon = {
-    url: "https://cdn4.iconfinder.com/data/icons/circle-map-pins/512/map_pin_destination_location_adress_train_metro_subway-512.png", // url
-    scaledSize: new google.maps.Size(65, 65), // scaled size
-    origin: new google.maps.Point(0,0), // origin
-    anchor: new google.maps.Point(0, 0) // anchor
-  };
-  var marker =  new google.maps.Marker({
-  	position: {lat: lat, lng: lan},
+function addStation(lat,lng) {
+  var latLng = {lat: lat, lng: lng};
+  if (stationMarkers === undefined || stationMarkers.length == 0 ) {
+    var icon = {
+    url: "https://static.thenounproject.com/png/733236-200.png", // url
+    scaledSize: new google.maps.Size(65, 65),   // scaled size
+    origin:     new google.maps.Point(0,0),    // origin
+    anchor:     new google.maps.Point(0,0)    // anchor
+    };
+    var marker =  new google.maps.Marker({
+  	position: latLng,
   	map: map,
-  	title: "Speed: "+speed,
+  	title: "Train Station",
   	icon: icon
   	});
-    markers.push(marker);
+    stationMarkers.push(marker);
   	marker.addListener("click",function() {
   	  var info = new google.maps.InfoWindow();
   		if (info.marker != this) {
@@ -64,4 +74,48 @@ function addMarker(lat,lan,speed) {
         });
       }
   	});
+  	map.panTo(latLng);
+  	map.setZoom(20);
+  	bounds = new google.maps.LatLngBounds();
+  }
+  stationMarkers[0].setPosition(latLng);
+  stationMarkers[0].setTitle("Train Station");
+}
+
+function addTrain(lat,lng,speed) {
+  var latLng = {lat: lat, lng: lng};
+  if(trainMarkers === undefined || trainMarkers.length == 0){
+  var icon = {
+    url: "https://cdn4.iconfinder.com/data/icons/circle-map-pins/512/map_pin_destination_location_adress_train_metro_subway-512.png", // url
+    scaledSize: new google.maps.Size(65, 65), // scaled size
+    origin: new google.maps.Point(0,0), // origin
+    anchor: new google.maps.Point(0, 0) // anchor
+  };
+  var marker =  new google.maps.Marker({
+  	position: latLng,
+  	map: map,
+  	title: "Speed: "+speed,
+  	icon: icon
+  	});
+    trainMarkers.push(marker);
+  	marker.addListener("click",function() {
+  	  var info = new google.maps.InfoWindow();
+  		if (info.marker != this) {
+        info.marker = this;
+        info.setContent('<div>' + this.title + '</div>');
+        info.open(map, this);
+        info.addListener('closeclick',function(){
+          info.setMarker = null;
+        });
+      }
+  	});
+  	if (stationMarkers === undefined || stationMarkers.length == 0 ) {
+  	  map.panTo(latLng);
+    	map.setZoom(20);
+  	  bounds = new google.maps.LatLngBounds();
+  	}
+  	
+  }
+  trainMarkers[0].setPosition(latLng);
+  trainMarkers[0].setTitle("Speed: "+speed);
 }
